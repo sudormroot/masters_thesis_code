128c9ec2ed62284fc953c792710388360852f7dc
sudormroot <unix.march@gmail.com>
2021-08-06 13:14:45 +0100

    v2

--- backup.sh
+++ backup.sh
@@ -2,9 +2,11 @@
 
 timestamp=`date "+%Y%m%d_%H%M%S"`
 
+version="v2"
+
 echo $timestamp
 
 mkdir bak 2>/dev/zero
 sync
-cp thesis_main.ipynb bak/thesis_main_$timestamp.ipynb 2>/dev/zero
+cp thesis_main_$version.ipynb bak/thesis_main_$version""_""$timestamp.ipynb 2>/dev/zero
 ls -l bak/*.ipynb
